54367e0e4c293def638e1d32df4278858dd9c063
Tartaare <74738404+Tartaare@users.noreply.github.com>
2026-05-24 22:17:44 +0200

    chore: remove large template files exceeding GitHub size limits

--- templates/night_vehicle_v01/templates/save_fork_sketchbook/templates/car_template_2/src/components/scene-objects/ObjectCollisionTypes.tsx
+++
@@ -1,5 +0,0 @@
-export const SURFACE_FOR_PAINT = 1;
-export const WHEEL = 2;
-export const PILLAR = 4;
-export const CHASSIS = 8;
-export const CUBE = 16;
--- templates/night_vehicle_v01/templates/save_fork_sketchbook/templates/car_template_2/src/components/scene-objects/RigidVehicle/RigidVehicle.tsx
+++
@@ -1,7 +0,0 @@
-// import {useRigidVehicle} from '@react-three/cannon'
-
-const RigidVehicle = () => {
-  // const [vehicle, api] = useRigidVehicle(() => raycastVehicleParams);
-
-  return <>Rigid Vehicle</>;
-};
